0c4626fd585518e0b16d6c6b700d15554c8d0a0f
bijay96 <bijayprakash516@gmail.com>
2024-08-05 13:05:21 +0800

    get_Number of unread pages and number of completed pages

--- bookcollection.py
+++ bookcollection.py
@@ -10,3 +10,18 @@ class BookCollection:
     def add_book(self, book):
         """Add a book to the collection."""
         self.books.append(book)
+
+    def get_number_of_unread_pages(self):
+
+        return sum(book.pages for book in self.books if not books.is_completed)
+
+    def get_number_of_completed_pages(self):
+        """Return the total number of completed pages."""
+        return sum(book.pages for book in self.books if book.is_completed)
+
+    def load_books(self, filename):
+        """Load books from a JSON file."""
+        with open(filename, 'r') as file:
+            books_data = json.load(file)
+            self.books = [Book(**data) for data in books_data]
+
