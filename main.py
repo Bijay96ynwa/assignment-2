82f9b42c95c7ba2aa90a6543027a8b944175701c
bijay96 <bijayprakash516@gmail.com>
2024-08-07 13:06:48 +0800

    kivy layout(app.kv)

--- main.py
+++ main.py
@@ -29,3 +29,57 @@ class BookApp(App):
              button.background_color = (0, 1, 0, 1) if book.is_completed else (1, 0, 0, 1)
              self.root.ids.books_box.add_widget(button)
          self.update_status()
+
+     def press_entry(self, instance):
+         """Handle pressing a book button."""
+         title = instance.text.split(" by ")[0]
+         for book in self.book_collection.books:
+             if book.title == title:
+                 if book.is_completed:
+                     book.mark_required()
+                 else:
+                     book.mark_completed()
+                 instance.background_color = (0, 1, 0, 1) if book.is_completed else (1, 0, 0, 1)
+                 self.update_status()
+                 break
+
+     def handle_add(self):
+         """Handle adding a new book."""
+         title = self.root.ids.input_title.text
+         author = self.root.ids.input_author.text
+         pages = self.root.ids.input_pages.text
+
+         if not title or not author or not pages:
+             self.status_text = "Please complete all fields."
+             return
+
+         try:
+             pages = int(pages)
+             if pages <= 0:
+                 self.status_text = "The book must have some pages!"
+                 return
+         except ValueError:
+             self.status_text = "Please enter a valid number."
+             return
+
+         new_book = Book(title, author, pages)
+         self.book_collection.add_book(new_book)
+         self.root.ids.input_title.text = ""
+         self.root.ids.input_author.text = ""
+         self.root.ids.input_pages.text = ""
+         self.create_book_buttons()
+         self.status_text = f"Book '{title}' added."
+
+     def update_status(self):
+         """Update the status labels."""
+         unread_pages = self.book_collection.get_number_of_unread_pages()
+         self.root.ids.status_label.text = f"Pages to read: {unread_pages}"
+
+     def on_stop(self):
+         """Save books when the app is closed."""
+         self.book_collection.save_books('books.json')
+
+
+if __name__ == '__main__':
+    BookApp().run()
+
